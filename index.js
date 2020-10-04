5408e6b12ca505390950c04352c7525377d1e6a7
stephenandersondev <stephenandersondev@gmail.com>
2020-10-04 15:41:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your solution here!
+const append = ["Milo", "Otis", "Garfield"],
+prepend = ["Milo", "Otis", "Garfield"],
+removeLast = ["Milo", "Otis", "Garfield"],
+removeFirst = ["Milo", "Otis", "Garfield"];
